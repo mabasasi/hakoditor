172bf867a42499e9d1c4aaefd9a0870f5b6e6063
mabasasi <aoe317293@yahoo.co.jp>
2018-02-27 00:39:12 +0900

    [add]タイトル付与

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.hakoditor')
+@section('title', $article->title)
 
 @section('container')
 
-    <h1>{{ $article->name ?? 'No Title' }}</h1>
+    <h1>{{ $article->title ?? 'No Title' }}</h1>
 
     {!! nl2br($article->html_content) !!}
 
