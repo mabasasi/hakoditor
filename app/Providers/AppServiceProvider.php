9276e456cb901bce8b10f681c623680cecaf01f1
mabasasi <aoe317293@yahoo.co.jp>
2018-02-27 00:26:40 +0900

    [add]バリデーター移植

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        // extend validator
+        \Validator::resolver(function ($translator, $data, $rules, $messages) {
+            return new DatabaseValidator($translator, $data, $rules, $messages);
+        });
+
+        \Form::macro('openResource', function(string $routeName, $id) {
+            $method = ($id) ? 'POST' : 'PUT';
+            $name   = ($id) ? 'store' : 'update';
+
+            $params = [];
+            if ($id) {
+                $params['id'] = $id;
+            }
+
+            return \Form::open(['method' => $method, 'url' => route($routeName.'.'.$name, $params)]);
+        });
+
     }
 
     /**
