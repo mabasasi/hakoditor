34dcb64986c1966f8620c11acacedc475a467896
mabasasi <aoe317293@yahoo.co.jp>
2018-02-26 21:38:25 +0900

    [fix]リレーションのパスの修正

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model {
 
     public function hakos() {
-        return $this->belongsToMany('App\Models\Article')->withPivot('order');
+        return $this->belongsToMany('App\Models\Hako')->withPivot('order');
     }
 
 }
