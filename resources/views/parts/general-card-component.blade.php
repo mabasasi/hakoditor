2fb9194e7dfb35c6a7f86bebcaebbfedd2c77952
mabasasi <aoe317293@yahoo.co.jp>
2018-02-26 22:40:25 +0900

    [add]blade パーツの移植

---
+++ resources/views/parts/general-card-component.blade.php
@@ -0,0 +1,15 @@
+
+{{--Heigit 最大化支援--}}
+@php($doExpand = isset($expand) ? $expand : false)
+
+<div class="card margin{{ out_if_true($doExpand, ' expand-height') }}{{ isset($class) ? ' '.$class : '' }}">
+    @isset($header)
+        <div class="card-header">
+            {{ $header }}
+        </div>
+    @endisset
+
+    <div class="card-body">
+        {{ $slot }}
+    </div>
+</div>
