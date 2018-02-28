ee11a5cb44c5977a5115680cb331ee4a6da67a49
mabasasi <aoe317293@yahoo.co.jp>
2018-02-28 09:17:52 +0900

    [add]管理テーブルを拡張表示に変更

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -16,37 +16,39 @@
     <div class="row">
         <div class="col">
 
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>タグ名</th>
-                    <th>親のタグID</th>
-                    <th>パス</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                @forelse($tags as $tag)
+            @component('parts.general-card-component', ['expand' => 'true'])
+                <table class="table table-hover">
+                    <thead>
                     <tr>
-                        <th>{{ $tag->id }}</th>
-                        <td>{{ $tag->name ?? '-' }}</td>
-                        <td>{{ $tag->parent_tag_id ?? '-' }}</td>
-                        <td>{{ '/'.$tag->tag_path ?? '-' }}</td>
-                        <td>
-                            <a href="{{ route('tags.edit', ['tag' => $tag->id]) }}" class="btn btn-tr btn-success" role="button" aria-pressed="true">
-                                <i class="fas fa-edit"></i> 編集
-                            </a>
-                            <a href="{{ route('tags.destroy', ['tag' => $tag->id]) }}" class="btn btn-tr btn-danger" role="button" aria-pressed="true">
-                                <i class="fas fa-trash"></i> 削除
-                            </a>
-                        </td>
+                        <th>#</th>
+                        <th>タグ名</th>
+                        <th>親のタグID</th>
+                        <th>パス</th>
+                        <th></th>
                     </tr>
-                @empty
-                    <p>No Data.</p>
-                @endforelse
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    @forelse($tags as $tag)
+                        <tr>
+                            <th>{{ $tag->id }}</th>
+                            <td>{{ $tag->name ?? '-' }}</td>
+                            <td>{{ $tag->parent_tag_id ?? '-' }}</td>
+                            <td>{{ '/'.$tag->tag_path ?? '-' }}</td>
+                            <td>
+                                <a href="{{ route('tags.edit', ['tag' => $tag->id]) }}" class="btn btn-tr btn-success" role="button" aria-pressed="true">
+                                    <i class="fas fa-edit"></i> 編集
+                                </a>
+                                <a href="{{ route('tags.destroy', ['tag' => $tag->id]) }}" class="btn btn-tr btn-danger" role="button" aria-pressed="true">
+                                    <i class="fas fa-trash"></i> 削除
+                                </a>
+                            </td>
+                        </tr>
+                    @empty
+                        <p>No Data.</p>
+                    @endforelse
+                    </tbody>
+                </table>
+            @endcomponent
 
         </div>
     </div>
