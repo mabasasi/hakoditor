6a28b74bde059dac2b40774edbfd667a0e1eb10e
mabasasi <aoe317293@yahoo.co.jp>
2018-03-01 11:05:50 +0900

    [add]ヘッダナビゲーション固定＆main 追加

--- resources/views/layouts/hakoditor.blade.php
+++ resources/views/layouts/hakoditor.blade.php
@@ -4,19 +4,22 @@
     <!-- Navivation Bar -->
     @includeIf('layouts.navbar')
 
-    @if(request('error') == 'true')
-        <!-- Debug Error View-->
-        @includeIf('layouts.error')
-    @endif
+    <main>
+        @if(request('error') == 'true')
+            <!-- Debug Error View-->
+            @includeIf('layouts.error')
+        @endif
 
-    <!-- Main Content -->
-    @if (View::hasSection('container'))
-        <div class="container">
-            @yield('container')
-        </div>
-    @elseif(View::hasSection('container-fluid'))
-        <div class="container-fluid">
-            @yield('container-fluid')
-        </div>
-    @endif
+        <!-- Main Content -->
+        @if (View::hasSection('container'))
+            <div class="container">
+                @yield('container')
+            </div>
+        @elseif(View::hasSection('container-fluid'))
+            <div class="container-fluid">
+                @yield('container-fluid')
+            </div>
+        @endif
+    </main>
+
 @endsection
